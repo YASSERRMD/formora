4f48038ac2535c32236b0564ba3120fe176e1db1
YASSERRMD <arafath.yasser@gmail.com>
2026-03-28 22:27:43 +0400

    phase_1: Add merge function and finalize CssProfile in profile.rs

--- formora-core/src/css/profile.rs
+++ formora-core/src/css/profile.rs
@@ -255,6 +255,144 @@ impl CssProfile {
 
         profile
     }
+
+    pub fn merge(base: &CssProfile, overrides: &CssProfile) -> CssProfile {
+        let mut merged = base.clone();
+
+        if !overrides.form_wrapper.is_empty() {
+            merged.form_wrapper = overrides.form_wrapper.clone();
+        }
+        if !overrides.form_title.is_empty() {
+            merged.form_title = overrides.form_title.clone();
+        }
+        if !overrides.form_description.is_empty() {
+            merged.form_description = overrides.form_description.clone();
+        }
+        if !overrides.step_wrapper.is_empty() {
+            merged.step_wrapper = overrides.step_wrapper.clone();
+        }
+        if !overrides.step_title.is_empty() {
+            merged.step_title = overrides.step_title.clone();
+        }
+        if !overrides.progress_bar_wrapper.is_empty() {
+            merged.progress_bar_wrapper = overrides.progress_bar_wrapper.clone();
+        }
+        if !overrides.progress_bar_fill.is_empty() {
+            merged.progress_bar_fill = overrides.progress_bar_fill.clone();
+        }
+        if !overrides.progress_bar_label.is_empty() {
+            merged.progress_bar_label = overrides.progress_bar_label.clone();
+        }
+        if !overrides.field_group.is_empty() {
+            merged.field_group = overrides.field_group.clone();
+        }
+        if !overrides.field_label.is_empty() {
+            merged.field_label = overrides.field_label.clone();
+        }
+        if !overrides.field_required_marker.is_empty() {
+            merged.field_required_marker = overrides.field_required_marker.clone();
+        }
+        if !overrides.field_help_text.is_empty() {
+            merged.field_help_text = overrides.field_help_text.clone();
+        }
+        if !overrides.field_error_message.is_empty() {
+            merged.field_error_message = overrides.field_error_message.clone();
+        }
+        if !overrides.input_text.is_empty() {
+            merged.input_text = overrides.input_text.clone();
+        }
+        if !overrides.input_email.is_empty() {
+            merged.input_email = overrides.input_email.clone();
+        }
+        if !overrides.input_number.is_empty() {
+            merged.input_number = overrides.input_number.clone();
+        }
+        if !overrides.input_textarea.is_empty() {
+            merged.input_textarea = overrides.input_textarea.clone();
+        }
+        if !overrides.input_select.is_empty() {
+            merged.input_select = overrides.input_select.clone();
+        }
+        if !overrides.input_date.is_empty() {
+            merged.input_date = overrides.input_date.clone();
+        }
+        if !overrides.input_file.is_empty() {
+            merged.input_file = overrides.input_file.clone();
+        }
+        if !overrides.input_range.is_empty() {
+            merged.input_range = overrides.input_range.clone();
+        }
+        if !overrides.input_range_value_display.is_empty() {
+            merged.input_range_value_display = overrides.input_range_value_display.clone();
+        }
+        if !overrides.input_checkbox_wrapper.is_empty() {
+            merged.input_checkbox_wrapper = overrides.input_checkbox_wrapper.clone();
+        }
+        if !overrides.input_checkbox.is_empty() {
+            merged.input_checkbox = overrides.input_checkbox.clone();
+        }
+        if !overrides.input_checkbox_label.is_empty() {
+            merged.input_checkbox_label = overrides.input_checkbox_label.clone();
+        }
+        if !overrides.input_radio_wrapper.is_empty() {
+            merged.input_radio_wrapper = overrides.input_radio_wrapper.clone();
+        }
+        if !overrides.input_radio.is_empty() {
+            merged.input_radio = overrides.input_radio.clone();
+        }
+        if !overrides.input_radio_label.is_empty() {
+            merged.input_radio_label = overrides.input_radio_label.clone();
+        }
+        if !overrides.multi_select_wrapper.is_empty() {
+            merged.multi_select_wrapper = overrides.multi_select_wrapper.clone();
+        }
+        if !overrides.multi_select_tag.is_empty() {
+            merged.multi_select_tag = overrides.multi_select_tag.clone();
+        }
+        if !overrides.multi_select_tag_remove.is_empty() {
+            merged.multi_select_tag_remove = overrides.multi_select_tag_remove.clone();
+        }
+        if !overrides.multi_select_dropdown.is_empty() {
+            merged.multi_select_dropdown = overrides.multi_select_dropdown.clone();
+        }
+        if !overrides.multi_select_option.is_empty() {
+            merged.multi_select_option = overrides.multi_select_option.clone();
+        }
+        if !overrides.input_error_state.is_empty() {
+            merged.input_error_state = overrides.input_error_state.clone();
+        }
+        if !overrides.input_valid_state.is_empty() {
+            merged.input_valid_state = overrides.input_valid_state.clone();
+        }
+        if !overrides.field_hidden.is_empty() {
+            merged.field_hidden = overrides.field_hidden.clone();
+        }
+        if !overrides.button_submit.is_empty() {
+            merged.button_submit = overrides.button_submit.clone();
+        }
+        if !overrides.button_next.is_empty() {
+            merged.button_next = overrides.button_next.clone();
+        }
+        if !overrides.button_back.is_empty() {
+            merged.button_back = overrides.button_back.clone();
+        }
+        if !overrides.button_wrapper.is_empty() {
+            merged.button_wrapper = overrides.button_wrapper.clone();
+        }
+        if !overrides.success_wrapper.is_empty() {
+            merged.success_wrapper = overrides.success_wrapper.clone();
+        }
+        if !overrides.success_message.is_empty() {
+            merged.success_message = overrides.success_message.clone();
+        }
+        if !overrides.error_wrapper.is_empty() {
+            merged.error_wrapper = overrides.error_wrapper.clone();
+        }
+        if !overrides.error_message.is_empty() {
+            merged.error_message = overrides.error_message.clone();
+        }
+        merged
+    }
 }
 
 impl Default for CssProfile {
