fc9d00ae9ce613aa0c4955d8368860cbae7f6796
YASSERRMD <arafath.yasser@gmail.com>
2026-03-28 21:53:12 +0400

    phase_7: Add Python package init and LLM utility functions

---
+++ formora-py/formora/__init__.py
@@ -0,0 +1,21 @@
+from .formora import (
+    Form,
+    Rule,
+    Condition,
+    CssFramework,
+    CssProfile,
+    FormResult,
+    parse_message as parse,
+    is_formora as is_formora_message,
+)
+
+__all__ = [
+    "Form",
+    "Rule",
+    "Condition",
+    "CssFramework",
+    "CssProfile",
+    "FormResult",
+    "parse",
+    "is_formora_message",
+]
